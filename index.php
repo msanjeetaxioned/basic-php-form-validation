6a4f86867a74863180a2f9ea642cf0a3611cfb6c
Sanjeet Mistry <sanjeetm@axioned.com>
2022-01-25 11:41:42 +0530

    Add Separate file for Validation functions

--- index.php
+++ index.php
@@ -14,88 +14,7 @@ $nameError = $emailError = $mobileNumError = $confirmPassError = $fileError = ""
 $passwordError = "Hint: Password should be Min. '8' in length and must contain Min. '1' Uppercase Charactor and '1' Number.";
 $name = $email = $mobileNum = $gender = $password = $confirmPass = $file = "";
 
-function nameValidation() {
-    GLOBAL $name;
-    if (strlen($name) >= 2) {
-        $pattern = "/^[a-z ,.'-]+$/i";
-        if (preg_match($pattern, $name)) {
-            return true;
-        }
-        return false;
-    }
-    return false;
-}
-
-function emailValidation() {
-    GLOBAL $email;
-    $pattern = "/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i";
-    if (preg_match($pattern, $email)) {
-        return true;
-    }
-    return false;
-}
-
-function mobileNumValidation() {
-    GLOBAL $mobileNum;
-    $pattern = "/^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/";
-    if (preg_match($pattern, $mobileNum)) {
-        return true;
-    }
-    return false;
-}
-
-function passwordValidation($password) {
-    $pattern = "/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/";
-    if (preg_match($pattern, $password)) {
-        return true;
-    }
-    return false;
-}
-
-function confirmPasswordValidation() {
-    GLOBAL $password;
-    GLOBAL $confirmPass;
-    if (passwordValidation($confirmPass) && $confirmPass == $password) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
-function fileValidation() {
-    GLOBAL $file;
-    GLOBAL $fileError;
-
-    $target_dir = "image-upload/";
-    $target_file = $target_dir.basename($file["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-    // Check if image file is a actual image or fake image
-    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-        $fileError =  "*Sorry, only JPG, JPEG & PNG Image files are allowed.";
-        return;
-    }
-
-    if (file_exists($target_file)) {
-        $fileError =  "*Sorry, file already exists. Try a different name.";
-        return;
-    }
-
-    if ($file["size"] > 1500000) {
-        $fileError =  "*Sorry, your file is too large. Max. allowed size <=1.5mb.";
-        return;
-    }
-
-    if (move_uploaded_file($file["tmp_name"], $target_file)) {
-        $fileError = "";
-        return;
-    } else {
-        $fileError = "*Sorry, some Unknown Error Occured.";
-        return;
-    }
-}
+require('form-validation.php');
 
 // print_r($_POST);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -194,6 +113,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </form>
         <?php
+        // After Successful form submit show message and submitted data to User.
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($nameError == "" && $emailError == "" && $mobileNumError == "" && $passwordError == "" && $confirmPassError == "" && $fileError == "") {
                 echo "<div class='submitted-data'>";
